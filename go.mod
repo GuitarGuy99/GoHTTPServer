848e80f3f2e4e6b02fcdb3928e89cdb9a3376e81
Oliver Porter-Smith <oliverps@live.com>
2024-03-30 15:53:40 +0000

    HTTP Server functioning at basic level

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/GuitarGuy99/GoHTTPServer
+
+go 1.22.1
+
+require (
+	github.com/go-chi/chi v1.5.5
+	github.com/joho/godotenv v1.5.1
+)
--- main.go
+++ main.go
@@ -1,10 +1,34 @@
 package main
 
-import "fmt"
-
+import( 
+"fmt"
+"os"
+"log"
+"net/http"
+"github.com/joho/godotenv"
+"github.com/go-chi/chi"
+)
 
 func main(){
  fmt.Println("Hello World")
+ godotenv.Load()
+ PortString := os.Getenv("PORT")
 
+
+ if PortString == ""{
+    log.Fatal("PORT is not set in the .env file")
+ }
+ router := chi.NewRouter()
+ srv := &http.Server{
+     Handler: router,
+     Addr:  ":" + PortString,
+     }
+
+ log.Printf("Server staring on port %v",PortString)
+ err := srv.ListenAndServe()
+ if err != nil {
+   log.Fatal(err)
+ }
+ fmt.Print("Port:",PortString)
 
 }
